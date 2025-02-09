ff2e8fbd211c3b001e56dfa855c10569e83f5afc
jakemarkwell <jake.markwell@hotmail.com>
2025-02-09 13:52:24 +0000

    Tidy Up

--- clipVault/Program.cs
+++ clipVault/Program.cs
@@ -10,12 +10,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddFastEndpoints();
 builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(GetThumbnailHandler).Assembly));
 
-// Config for MP4 Files
-builder.Services.Configure<FormOptions>(options =>
-{
-    options.MultipartBodyLengthLimit = 104857600; // 100MB
-});
-
 builder.Services.AddSingleton(new BlobServiceClient(builder.Configuration.GetConnectionString("AzureBlobStorage")));
 
 var app = builder.Build();
